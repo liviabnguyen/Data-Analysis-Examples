e8cd395e1321ec1d7f90ef3b6a1d8ebec85c8ac3
lnguy091 <63258869+lnguy091@users.noreply.github.com>
2020-09-01 17:42:08 -0700

    first commit

--- Buscos/kmersinbuscos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-module load jellyfish/2.2.9
-
-INPUT=A_thal_single_copy_buscos.fa
-OUTPUT=kmers_in_buscos.txt.gz 
-
-jellyfish count -C -m 12 -s 500M -o temp.jf "$INPUT"
-jellyfish dump -tc temp.jf | gzip > "$OUTPUT"
--- Buscos/srun_example.sh
+++
@@ -1,2 +0,0 @@
-# this gets you 64 GB of RAM with 4 threads for 2 hours 
-srun --x11 --partition=koeniglab --mem=64gb --cpus-per-task 4 --ntasks=1 --time 2:00:00 --pty bash -l
